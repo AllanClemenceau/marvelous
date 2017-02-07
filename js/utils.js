dbc520c85e40acdf9a201bb86158a8fdd41f36ad
AllanClemenceau <clemenceau.all@gmail.com>
2017-02-07 01:16:14 +0100

    animtion en css et plus avec gsap

--- js/utils.js
+++ js/utils.js
@@ -12,6 +12,7 @@ redPanel = $("#red-panel");
 title = $("#main-container .title");
 titleLa = $("#main-container .title-la");
 titleOus = $("#main-container .title-ous");
+
 isTitleVisible = false;
 isStapeOne = false;
 isStapeTwo = false;
@@ -46,6 +47,7 @@ pages = [
     "http://www.can-developing.com/marvelous/img/marvelScreen23.jpg",
     "http://www.can-developing.com/marvelous/img/marvelScreen24.jpg",
 ];
+
 function simulateResize() {
     var evt = document.createEvent('UIEvents');
     evt.initUIEvent('resize', true, false, window, 0);
@@ -56,51 +58,64 @@ t = new TimelineLite({paused:true});
 
 function loopPages(url, i, array){
     var startingPoint = (i*0.5 - (0.5/array.length)*i*10);
-    var page = $("<div/>", {
-      class : "page",
-      style: "background-image:url('" + url + "');background-repeat:no-repeat;background-size:cover"
-    }).appendTo(container);
+    // var page = $("<div/>", {
+    //   class : "page",
+    //   style: "background-image:url('" + url + "');background-repeat:no-repeat;background-size:cover"
+    // }).appendTo(container);
 
-    if (i > (array.length/(5/4)) && !isTitleVisible) {
-        isTitleVisible = true;
-        startingPoint = (i*0.5 - (0.5/array.length)*i*16);
-        t.fromTo(titleLa, 0.5, {opacity: 0}, {opacity: 1}, startingPoint);
-        t.fromTo(titleOus, 0.5, {opacity: 0}, {opacity: 1}, startingPoint);
-    }  else if (i > (array.length/(3/2))) {
-        isStapeThree = true;
-        startingPoint = (i*0.5 - (0.5/array.length)*i*16);
-        t.to(redPanel, 0.5, {opacity: 1, ease:SlowMo.easeOut}, startingPoint);
-    } else if (i > (array.length/3)) {
-        startingPoint = (i*0.5 - (0.5/array.length)*i*14);
-        if (i < (array.length/(3/2))) {
-            t.fromTo(title, 1, {opacity: 0}, {opacity: 1}, startingPoint);
-        }
-        t.to(redPanel, 0.5, {opacity: 0.75, ease:SlowMo.easeOut}, startingPoint);
-    } else if (i > (array.length/(1/3))) {
-        startingPoint = (i*0.5 - (0.5/array.length)*i*12);
-    } else if (i == 0) {
-        t.to(redPanel, 1.5, {opacity: 0.5, backgroundColor: '#e23636', ease:SlowMo.easeOut}, 0.3);
-    }
+    // if (i > (array.length/(5/4)) && !isTitleVisible) {
+    //     isTitleVisible = true;
+    //     startingPoint = (i*0.5 - (0.5/array.length)*i*16);
+    //     t.fromTo(titleLa, 0.5, {opacity: 0}, {opacity: 1}, startingPoint);
+    //     t.fromTo(titleOus, 0.5, {opacity: 0}, {opacity: 1}, startingPoint);
+    // }  else if (i > (array.length/(3/2))) {
+    //     isStapeThree = true;
+    //     startingPoint = (i*0.5 - (0.5/array.length)*i*16);
+    //     t.to(redPanel, 0.5, {opacity: 1, ease:SlowMo.easeOut}, startingPoint);
+    // } else if (i > (array.length/3)) {
+    //     startingPoint = (i*0.5 - (0.5/array.length)*i*14);
+    //     if (i < (array.length/(3/2))) {
+    //         t.fromTo(title, 1, {opacity: 0}, {opacity: 1}, startingPoint);
+    //     }
+    //     t.to(redPanel, 0.5, {opacity: 0.75, ease:SlowMo.easeOut}, startingPoint);
+    // } else if (i > (array.length/(1/3))) {
+    //     startingPoint = (i*0.5 - (0.5/array.length)*i*12);
+    // } else if (i == 0) {
+    //     t.to(redPanel, 1.5, {opacity: 0.5, backgroundColor: '#e23636', ease:SlowMo.easeOut}, 0.3);
+    // }
+    $('#marvelImages > .marvelImage:nth-child(' + i + ')').addClass('translate');
+    $('#red-panel, #main-container .title, #main-container .title-la, #main-container .title-ous').addClass('fadeIn');
 
-    t.to(page, (0.5 - (0.5/array.length)*i), {transform: 'translateY(100vh)', ease:Power1.easeOut}, startingPoint);
+    // t.to($('#marvelImages > .marvelImage:nth-child(' + i + ')'), (0.5 - (0.5/array.length)*i), {transform: 'translateY(100vh)', z:0.1, ease:Power1.easeOut}, startingPoint);
     // console.log((i*0.5 - (0.5/array.length)*i));
 }
 
 function addPages(){
-  pages.forEach(loopPages);
-  t.to(title, 2, {transform: 'scale(0.15)', top: '-50%'}, 'end');
-  t.to('#headerSvg > path' , 0, {fill: '#e23636'}, 'end');
-  t.fromTo('#headerSvg', 2, {top: '-500px'}, {top : '0'}, 'end');
-  t.fromTo('#header > .title', 2, {top: '-500px'}, {top : '15%'}, 'end');
-  t.to('#headerSvg > path', 2.5, {clearProps: 'fill'}, 'end');
-  t.to(container, 1, {opacity: '0'}, 'end');
-  t.to(container, 1, {display: 'none'}, 'end');
-  t.to("#header > svg#star > path", 0, {fill: '#e23636', stroke: '#fff', strokeWidth: '1', ease: Power1.easeIn}, 'end');
-  t.fromTo('#header > svg#star', 2, {top: '100vh'}, {rotation: '+=1080', top : '100px'}, 'end');
+  $('#marvelImages > .marvelImage').addClass('translate');
+  $('#red-panel, #main-container .title, #main-container .title-la, #main-container .title-ous').addClass('fadeIn');
+  setTimeout(function(){
+  t.to(title, 2, {transform: 'scale(0.15)', top: '-50%'}, 0);
+  t.to('#headerSvg > path' , 0, {fill: '#e23636'}, 0);
+  t.fromTo('#headerSvg', 2, {top: '-500px'}, {top : '0'}, 0);
+  t.fromTo('#header > .title', 2, {top: '-500px'}, {top : '15%'}, 0);
+  t.to('#headerSvg > path', 2.5, {clearProps: 'fill'}, 0);
+  t.to(container, 1, {opacity: '0'}, 0);
+  t.to(container, 1, {display: 'none'}, 0);
+  t.to("#header > svg#star > path", 0, {fill: '#e23636', stroke: '#fff', strokeWidth: '1', ease: Power1.easeIn}, 0);
+  t.fromTo('#header > svg#star', 2, {top: '100vh'}, {rotation: '+=1080', top : '100px'}, 0);
   // t.to("#header > svg#star > path", 0.25, {fill: '#20346F', stroke: '#20346F', ease: Power1.easeOut});
   t.to("#header > svg#star > path", 0.5, {clearProps: 'fill, stroke, strokeWidth', onComplete:simulateResize()});
   // t.to(container, 0, {display: 'none'});
   t.play();
+      // $('#headerSvg > path,
+      //     #headerSvg,
+      //     #header > .title,
+      //     #headerSvg > path,
+      //     #main-container,
+      //     #header > svg#star > path,
+      //     #header > svg#star
+      // ').addClass('phaseTwo');
+  }, 6640);
 }
 
 function parallax(e, target, layer) {
@@ -114,51 +129,57 @@ function parallax(e, target, layer) {
 $('.heroMap').click(function() {
     var id = $(this).attr('id').replace("map-", "");
     var t2 = new TimelineLite();
-    var t3 = new TimelineLite();
     if ($('#menu').hasClass('menu--open')) {
         $('#menu .trigger').click();
     }
-    t3.to('.morph-shape svg path', 1, {fill: '#20346F', ease: Power1.easeIn});
-    t3.to('.trigger', 1, {color: '#FFF', ease: Power1.easeIn}, 'start');
-    t3.to('.menu__items li a', 1, {color: '#FFF', background: '#20346F', ease: Power1.easeIn});
-    t2.to('#' + id, 1, {transform: 'scale(20)', filter: 'brightness(0) invert(1)', ease: Power1.easeIn})
+    t2.to('.morph-shape svg path', 1, {fill: '#20346F', ease: Power1.easeIn}, 0);
+    t2.to('.trigger', 1, {color: '#FFF', ease: Power1.easeIn}, 0);
+    t2.to('.menu__items li a', 0, {color: '#FFF', background: '#20346F', ease: Power1.easeIn}, 1);
+    t2.to('#' + id, 1, {transform: 'scale(20)', z:0.1, filter: 'brightness(0) invert(1)', ease: Power1.easeIn}, 0)
     t2.from('#' + id + 'Box', 0, {top: '0', display: 'inherit', opacity: '0'})
-        .to('#' + id + 'Box', 0.5, {top: '0', display: 'inherit', opacity: '1'});
+        .to('#' + id + 'Box', 0.5, {top: '0', display: 'inherit', opacity: '1'}, 1);
 });
 
 $('.close').click(function() {
     var id = $(this).parent().attr('id').replace("Box", "");
-    var t4 = new TimelineLite();
-    var t5 = new TimelineLite();
+    var t3 = new TimelineLite();
     if ($('#menu').hasClass('menu--open')) {
         $('#menu .trigger').click();
     }
-    t5.to('.morph-shape svg path', 1, {clearProps: 'fill', ease: Power1.easeIn});
-    t5.to('.trigger', 1, {color: '#20346F', ease: Power1.easeIn}, 'start');
-    t5.to('.trigger', 0, {clearProps: 'color', ease: Power1.easeIn});
-    t5.to('.menu__items li a', 1, {clearProps: 'color, background', ease: Power1.easeIn});
-    t4.from('#' + id + 'Box', 0, {top: '0', display: 'inherit', opacity: '1'})
-        .to('#' + id + 'Box', 0.5, {top: '0', display: 'none', opacity: '0'});
-    t4.to('#' + id, 1, {transform: 'scale(1)', ease: Power1.easeOut})
-      .to('#' + id, 0, {clearProps: 'filter, transform'});
+    t3.to('.morph-shape svg path', 1, {clearProps: 'fill', ease: Power1.easeIn}, 0);
+    t3.to('.trigger', 1, {color: '#20346F', ease: Power1.easeIn}, 0);
+    t3.to('.trigger', 0, {clearProps: 'color', ease: Power1.easeIn}, 1);
+    t3.to('.menu__items li a', 0, {clearProps: 'color, background', ease: Power1.easeIn}, 1);
+    t3.from('#' + id + 'Box', 0, {top: '0', display: 'inherit', opacity: '1'})
+        .to('#' + id + 'Box', 0.5, {top: '0', display: 'none', opacity: '0'}, 0);
+    t3.to('#' + id, 1, {transform: 'scale(1)', z:0.1, ease: Power1.easeOut}, 0.5)
+      .to('#' + id, 0, {clearProps: 'filter, transform'}, 1.5);
 });
 
 $(function() {
+    var x = 0.5;
+    var y = 0.5;
+    for (i=0;i<=24; i++) {
+      console.log(x);
+      y = y - 0.1;
+        // console.log(y);
+      x = Math.round((x + y) * 100) / 100;
+    }
     Pace.on('done', function() {
         addPages();
     });
-    $('#scene').mousemove(function (e) {
-        parallax(e, this, 2);
-        // parallax(e, document.getElementById('layer-two'), 2);
-        // parallax(e, document.getElementById('layer-three'), 3);
-    });
+    // if ('ontouchstart' in window || navigator.maxTouchPoints) {
+        $('#scene').mousemove(function (e) {
+            parallax(e, this, 2);
+            // parallax(e, document.getElementById('layer-two'), 2);
+        });
+    // }
 
     $('map').imageMapResize();
 });
 
 
 (function() {
-
 	function SVGMenu(el, options) {
 		this.el = el;
 		this.init();
@@ -200,16 +221,5 @@ $(function() {
 
 		this.isOpen = !this.isOpen;
 	};
-
 	new SVGMenu( document.getElementById('menu'));
-
 })();
-
-paceOptions = {
-  ajax: true, // disabled
-  document: true, // disabled
-  eventLag: true, // disabled
-  // elements: {
-  //   selectors: ['.my-page']
-  // }
-};
